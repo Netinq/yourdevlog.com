b181739db4e3d00abe5ddd7f0ff6f49cdb520f7c
Quentin Sar <48774470+Netinq@users.noreply.github.com>
2020-03-14 17:25:55 +0100

    Website page update

--- resources/views/website/show.blade.php
+++ resources/views/website/show.blade.php
@@ -2,8 +2,36 @@
 
 @section('title', $website->name)
 @section('content')
+<div class="offset-2 informations d-block d-md-none">
+  <div class="website">
+    <div class="box-web">
+      <div class="box-header">
+        <h3>{{$website->name}}</h3>
+        <span>{{$website->url}}</span>
+      </div>
+      <div class="box-body">
+        <div class="info">
+            <h4>{{count($articles)}} articles</h4>
+        </div>
+        <div class="info">
+            <h4>data uuid</h4>
+            <code class="uuid">{{$website->id}}</code>
+        </div>
+        <div class="info">
+          <h4>iframe</h4>
+          <code class="iframe" type="html">&lt;iframe id="yourdevlog_iframe" 
+title="YourDevlog : {{$website->name}}"
+width="100%" height="100%" allowfullscreen=true style="border: none;" 
+src="{{ route('data.show', $website->id) }}"&gt
+</iframe>
+          </code>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <div class="col-md-7 articles">
-  <div class="articles-content col-md-10 offset-2">
+  <div class="articles-content col-md-10 col-10 offset-2 offset-md-2">
     @foreach($articles as $article)
     <div class="article" data-article-id="{{ $article->id }}">
       <div class="article-header">
@@ -18,7 +46,7 @@
     @endforeach
   </div>
 </div>
-<div class="col-md-5 informations">
+<div class="col-md-5 offset-2 offset-md-0 informations d-none d-md-block">
   <div class="website">
     <div class="box-web">
       <div class="box-header">
