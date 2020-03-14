38e29ec40ed42f08bc100746d4c53f74d5848c28
Quentin Sar <48774470+Netinq@users.noreply.github.com>
2020-03-14 16:46:44 +0100

    Fixing devlog

--- resources/views/devlog.blade.php
+++ resources/views/devlog.blade.php
@@ -9,7 +9,7 @@
     <a href="{{route('home')}}"><div class="btn btn-home">Back to home</div></a>
 </div>
 <div class="articles-content col-sm-8">
-  <iframe id="yourdevlog_iframe" title="YourDevlog : YourDevlog" width="100%" height="100%" allowfullscreen=true style="border: none;" src="http://127.0.0.1:8000/data/31ff85f2-2c06-45fe-b863-a15b26f7979d">
+  <iframe id="yourdevlog_iframe" title="YourDevlog : YourDevlog" width="100%" height="100%" allowfullscreen=true style="border: none;" src="http://127.0.0.1:8000/data/{{$data_url}}">
 </div>
 @endsection
 
