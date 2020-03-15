b9debcd7478f13095233b687583dc9b878ac05b0
Quentin Sar <48774470+Netinq@users.noreply.github.com>
2020-03-15 03:06:30 +0100

    Fixing devlog

--- resources/views/devlog.blade.php
+++ resources/views/devlog.blade.php
@@ -24,5 +24,3 @@
     @endforeach
 </div>
 @endsection
-
-{{-- <iframe id="yourdevlog_iframe" title="YourDevlog : YourDevlog" width="100%" height="100%" allowfullscreen=true style="border: none;" src="http://127.0.0.1:8000/data/31ff85f2-2c06-45fe-b863-a15b26f7979d"> --}}
