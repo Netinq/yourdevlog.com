c8c7f0d5990250c1970ee6707c859e47b606ca34
Quentin Sar <48774470+Netinq@users.noreply.github.com>
2020-03-27 16:31:43 +0100

    Data fixing

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -3,8 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   </head>
 <body>
   <div id="app">
